021f9a80fc13421a3f15b9e9b6ec6dcde6e45ff9
sheriff-kurdi <sheriff.kurdi@gmail.com>
2021-10-18 15:40:04 +0200

    init

--- src/main/java/com/kurdi/redis/RedisApplication.java
+++ src/main/java/com/kurdi/redis/RedisApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//i
+//is
 public class RedisApplication {
 
 	public static void main(String[] args) {
