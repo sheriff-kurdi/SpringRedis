8f150537d36dbdc70bf1156586b5f3d6da97e812
sheriff-kurdi <sheriff.kurdi@gmail.com>
2021-10-18 15:55:13 +0200

    redis config

--- src/main/java/com/kurdi/redis/config/RedisConfig.java
+++ src/main/java/com/kurdi/redis/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.kurdi.redis.config;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
     @Bean
     public JedisConnectionFactory jedisConnectionFactory(){
